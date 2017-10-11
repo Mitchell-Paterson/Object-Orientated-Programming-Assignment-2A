f66abf3f2d257e4905f6b79ff22a11e006e85e7f
Mitchell Paterson <30946820+Red-Toast@users.noreply.github.com>
2017-10-11 19:40:55 +1100

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -4,5 +4,17 @@ public abstract class Enemy extends Movable {
 	public Enemy(String image_src, Coordinate coordinate, World world) {
 		super(image_src, coordinate, world);
 	}
+	
+	/* 
+	
+	Sometimes I wish real life was more like programming.
+	
+	Run a program, it crashes.
+	Run it again 5 minutes later, it works.
+	
+	Open the fridge, no food.
+	Check again 5 minutes later, food!
+	
+	*/
 
 }
